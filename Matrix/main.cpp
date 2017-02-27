05681e60aefec92b707aa7ec745d45af92e23378
marek <marzi@marzi.(none)>
2017-02-27 23:41:14 +0100

    Matrix ver 0.1

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
 #include "matrix.hpp"
 
+struct P {
+public:
+	P() = default;
+	P(const P&) = default;
+	P(P&&) = delete;
+
+private:
+	unsigned x = 0;
+};
+
+template <typename Type, size_t size>
+class Vec {
+public:
+	Vec(std::array<Type, size>& data) : data(data) {}
+	Vec(const Vec& other) = default;
+	Vec(Vec&& other) = default;
+	virtual ~Vec() = default;
+
+	virtual Type& operator[](size_t pos) noexcept { return data[pos]; }
+	virtual const Type& operator[](size_t pos) const noexcept { return data[pos]; }
+
+protected:
+	std::array<Type, size> data;
+};
+
+template <typename Type>
+class Position : public Vec<Type, 4> {
+public:
+	Position(Type x, Type y, Type z, Type w = 1)
+			: Vec<Type, 4>({x, y, z, w}),
+			  x(this->data[0]),
+			  y(this->data[1]),
+			  z(this->data[2]),
+			  w(this->data[3]) {}
+
+	Type& x, y, z, w;
+};
+
+template <typename Type, size_t size>
+class Color : public Vec<Type, 4> {
+	Color(Type r, Type g, Type b, Type a = 1)
+			: Vec<Type, 4>({ r, g, b, a }),
+			  r(this->data[0]),
+			  g(this->data[1]),
+			  b(this->data[2]),
+			  a(this->data[3]) {}
+
+	Type& r, g, b, a;
+};
+
 int main() {
 	Matrix<int, 3, 6> m;
-	m.at(2, 3) = 100;
+	Matrix<int, 1, 1> n = { 69 };
+//	Matrix<P, 2, 2> o = { P(), P(), P(), P()};
+	Matrix<unsigned, 3, 3> p;
+	p = { 1, 0, 0,
+		  0, 1, 0,
+		  0, 0, 1 };
+	p.at(2, 2) = 100;
+	const decltype(p)& r = p;
+	r.at(2,2);
+	r.swap(p);
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
--- Matrix/matrix.hpp
+++ Matrix/matrix.hpp
@@ -13,39 +13,141 @@ class Matrix {
 	using size_type = std::size_t;
 
 public:
+	/**
+	 * @brief 	default ctor
+	 */
 	Matrix() = default;
-	Matrix(std::initializer_list<Type&&> list) {
-		if (list.size() != size())
+
+	/**
+	 * @brief 	copy ctor
+	 * @param 	m
+	 */
+	Matrix(const Matrix& m) : _elements(m._elements) {}
+
+	/**
+	 * @brief	move ctor
+	 * @param 	m
+	 */
+	Matrix(Matrix&& m) noexcept : _elements(std::move(m._elements)) {}
+
+	/**
+	 * @brief	initializer_list ctor
+	 * @param 	init
+	 */
+	Matrix(std::initializer_list<Type> init) {
+		if (init.size() != size())
 			throw std::logic_error("Wrong number of arguments given.");
-		for (size_type i = 0; i < list.size(); ++i)
-			_elements[i] = std::move(list[i]);
+		size_type i = 0;
+		for (auto&& e : init) {
+			_elements[i] = e;
+			++i;
+		}
 	}
 
-	Matrix(const Matrix&);
-	Matrix(Matrix&&);
+	/**
+	 * @brief	copy assigment operator
+	 * @param 	m
+	 * @return 	reference to this instance
+	 */
+	Matrix& operator=(const Matrix& m) {
+		_elements = m._elements;
+		return *this;
+	}
 
+	/**
+	 * @brief	move assigment operator
+	 * @param 	m
+	 * @return 	reference to this instance
+	 */
+	Matrix& operator=(Matrix&& m) noexcept {
+		_elements = std::move(m._elements);
+		return *this;
+	}
+
+	/**
+	 * @brief	initializer_list assigment operator
+	 * @param 	init
+	 * @return 	reference to this instance
+	 */
+	Matrix& operator=(std::initializer_list<Type> init) {
+		*this = Matrix(init);
+		return *this;
+	}
+
+	/**
+	 * @brief	swaps with other Matrix
+	 * @param 	m
+	 */
+	void swap(Matrix& m) noexcept {
+		Matrix tmp = std::move(m);
+		m = std::move(*this);
+		*this = std::move(tmp);
+	}
+
+	/**
+	 * @brief	size of matrix getter (width * height)
+	 * @return 	size of matrix
+	 */
 	size_type size() const noexcept { return _elements.size(); }
+
+	/**
+	 * @brief	width of matrix getter
+	 * @return 	widht of matrix
+	 */
 	size_type width() const noexcept { return _width; }
+
+	/**
+	 * @brief	height of matrix getter
+	 * @return 	heirght of matrix
+	 */
 	size_type height() const noexcept { return _height; }
 
-	reference operator()(size_type x, size_type y) noexcept { return _elements[x + y * _width]; }
-	const_reference operator()(size_type x, size_type y) const noexcept { return _elements[x + y * _width]; }
+	/**
+	 * @brief	function returning reference to element of matrix
+	 * @param 	x		position in column
+	 * @param 	y		position in row
+	 * @return	reference to element
+	 */
+	reference operator()(size_type x, size_type y) noexcept {
+		return _elements[x + y * _width];
+	}
 
+	/**
+	 * @brief	function returning const reference to element of matrix
+	 * @param 	x 		position in column
+	 * @param 	y 		position in row
+	 * @return 	const reference to element
+	 */
+	const_reference operator()(size_type x, size_type y) const noexcept {
+		return _elements[x + y * _width];
+	}
+
+	/**
+	 * @brief	function returning reference to element of matrix
+	 * 			std::out_of range throw is element is out of bounds
+	 * @param 	x		position in column
+	 * @param 	y		position in row
+	 * @return	reference to element
+	 */
 	reference at(size_type x, size_type y) {
 		if (x >= _width || y >= _height)
 			throw std::out_of_range("Given index is outside the matrix boundaries.");
 		return _elements.at(x + y * _width);
 	}
 
+	/**
+	 * @brief	function returning const reference to element of matrix
+	 * 			std::out_of range throw is element is out of bounds
+	 * @param 	x 		position in column
+	 * @param 	y 		position in row
+	 * @return 	const reference to element
+	 */
 	const_reference at(size_type x, size_type y) const {
-		if (x >= _width || y >= _height)
-			throw std::out_of_range("Given index is outside the matrix boundaries.");
-		return _elements.at(x + y * _width);
+		return const_cast<const_reference>(const_cast<Matrix*>(this)->at(x, y));
 	}
 
 private:
 	std::array<Type, _width * _height> _elements = std::array<Type, _width * _height>();
-	//std::vector<std::vector<Type> > _elements = std::vector<std::vector<Type> >(_height, std::vector<Type>(_width)) ;
 };
 
 #endif //MATRIX_MATRIX_HPP
